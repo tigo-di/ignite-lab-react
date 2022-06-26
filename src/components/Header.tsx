3543a1c351ba8b3104668681511bc44aa37f3a5d
Tiago Dias <tigo@webdeleve.net>
2022-06-26 15:56:38 -0300

    aula 2

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Logo } from "./logo";
+
+export function Header() {
+
+  return (
+
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-d border-gray-600">
+      <Logo />
+    </header>
+
+  )
+
+}
